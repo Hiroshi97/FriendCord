b079715dd26cd5f6b8a2827962e89d32dd33e346
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-09-15 04:54:21 +1000

    Delete comments

--- server/utils/create-unique-id.js
+++ server/utils/create-unique-id.js
@@ -1,10 +1,4 @@
-const createUniqueID = (_id1, _id2) => {
-    // let time1 = _id1.getTimestamp().getTime();
-    // let time2 = _id2.getTimestamp().getTime();
-    // let last3bytes1 = _id1.toString().slice(-3);
-    // let last3bytes2 = _id2.toString().slice(-3);
-    // if (time1===time2)
-     
+const createUniqueID = (_id1, _id2) => {   
     return _id1.toString().localeCompare(_id2.toString()) === -1 ? _id1 + _id2 : _id2 + _id1;
 }
 
