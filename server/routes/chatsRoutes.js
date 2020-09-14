ae203ca22d136640118e82f196b48c8e4ecb221d
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-09-15 04:49:51 +1000

    Check authorization and request length to make sure no conflict

--- server/routes/chatsRoutes.js
+++ server/routes/chatsRoutes.js
@@ -4,24 +4,23 @@ const User = require("../models/userModel");
 const Message = require("../models/messageModel");
 
 //ROUTES
-// /*Send a message*/
-
-// router.post("/send", async (req, res) => {
-//   try {
-      
-//   } catch (error) {
-//     return res.status(500).json({ error: error.message });
-//   }
-// });
 
 router.post("/getMessages", async (req, res) => {
   try {
-    console.log("ABC");
-      const {id1, id2} = req.body;
-      console.log(id1);
-      const messages = await Message.find({$or: [{from: id1, to: id2}, {from: id2, to: id1}]}).sort({created_at: 1});
-      console.log(messages);
-      return res.status(200).json({messages});
+    if (
+      req.headers.authorization === req.body.id1 + req.body.id1.slice(-3) ||
+      req.headers.authorization === req.body.id2 + req.body.id2.slice(-3)
+    ) {
+      console.log("ABC");
+      const { id1, id2 } = req.body;
+      const messages = await Message.find({
+        $or: [
+          { from: id1, to: id2 },
+          { from: id2, to: id1 },
+        ],
+      }).sort({ created_at: 1 });
+      return res.status(200).json({ messages });
+    } else return res.status(500).json("Unauthorized");
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -13,17 +13,20 @@ router.get("/all", async (req, res) => {
 });
 
 router.post("/friends", async (req, res) => {
+  console.log(req.headers);
   try {
-    console.log("ALO", req.body);
-    const user = await User.findOne({...req.body });
-    User.getAcceptedFriends(
-      user,
-      {},
-      { name: 1, username: 1, avatar: 1, email: 1 },
-      (err, friendships) => {
-        return res.status(200).json({ friendships });
-      }
-    );
+    if (Object.keys(req.body).length) {
+      const user = await User.findOne({ ...req.body }, { _id: 1, name: 1, username: 1, avatar: 1, email: 1 });
+
+      User.getAcceptedFriends(
+        user,
+        {},
+        { _id: 1, name: 1, username: 1, avatar: 1, email: 1 },
+        (err, friendships) => {
+          return res.status(200).json({ friendships });
+        }
+      );
+    } else return res.status(500).json("Empty request!");
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
@@ -31,30 +34,6 @@ router.post("/friends", async (req, res) => {
 
 router.get("/suggestions", async (req, res) => {
   try {
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
-});
-
-router.post("/test", async (req, res) => {
-  try {
-    console.log("ABC");
-    console.log("5f5a54257ccdd32d51ce41f7" ^ "5f5c19990953da70dc77eea0");
-    const user = await User.findOne({ _id: "5f5a54257ccdd32d51ce41f7" });
-    const user2 = await User.findOne({ _id: "5f5c19990953da70dc77eea0" });
-    console.log(user._id.getTimestamp().getTime());
-    console.log(new Date().getTime());
-    await User.requestFriend(user._id, user2._id, () => {});
-    await User.requestFriend(user2._id, user._id, () => {});
-    await User.getAcceptedFriends(
-      user,
-      {},
-      { name: 1, username: 1, avatar: 1, email: 1 },
-      (err, friendships) => {
-        return res.status(200).json({ friendships });
-      }
-    );
-    
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
