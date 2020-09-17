f1dfb00bae54b9556329233d227f927dcf0e83e9
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-09-18 02:42:50 +1000

    Display notification if the selected user has recently sent a friend request

--- client/src/components/ChatWindow/index.jsx
+++ client/src/components/ChatWindow/index.jsx
@@ -1,5 +1,14 @@
-import React, { useEffect, useRef } from "react";
-import { Row, Col, Form, Button, InputGroup, Card, Image } from "react-bootstrap";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Row,
+  Col,
+  Form,
+  Button,
+  InputGroup,
+  Card,
+  Image,
+  Toast,
+} from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPaperPlane,
@@ -12,28 +21,56 @@ import MessageCard from "./MessageCard";
 import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
 
-const ChatWindow = ({ selectedFriend, handleSubmitChatMessage, messages}) => {
+const ChatWindow = ({
+  selectedFriend,
+  handleSubmitChatMessage,
+  messages,
+  handleAddFriend,
+  handleCancelFriend,
+}) => {
   const userInfo = JSON.parse(localStorage.getItem("user"));
+  const [showToast, setShowToast] = useState(
+    selectedFriend && selectedFriend.status === "pending"
+  );
   const lastMessage = useRef(null);
   const prevLengthRef = useRef(messages.length);
   const containerRef = useRef(null);
+
   useEffect(() => {
-    if(lastMessage.current)
+    if (lastMessage.current)
       lastMessage.current.scrollIntoView({ behavior: "auto" });
   }, [messages]);
+
   const handleSubmitMessage = (e) => {
     handleSubmitChatMessage(e);
   };
 
+  const handleCloseToast = () => {
+    setShowToast(false);
+  };
+
+  const handleClickAddFriend = (id1, id2) => {
+    handleAddFriend(id1, id2);
+    handleCloseToast();
+  };
+
+  const handleClickCancelFriend = (id1, id2) => {
+    handleCancelFriend(id1, id2);
+    handleCloseToast();
+  };
+
   return (
     <>
-      {selectedFriend && (
         <Col className="text-center chat-window">
           <Card>
             <Card.Header>
               <Row>
                 <div className="avatar-wrapper">
-                  <Image src={selectedFriend.friend.avatar} alt="" roundedCircle/>
+                  <Image
+                    src={selectedFriend.friend.avatar}
+                    alt=""
+                    roundedCircle
+                  />
                   <span className="status"></span>
                 </div>
                 <div className="username-wrapper mt-auto pl-2 text-left">
@@ -42,7 +79,12 @@ const ChatWindow = ({ selectedFriend, handleSubmitChatMessage, messages}) => {
                   </span>
                   <p>{selectedFriend.friend.email}</p>
                 </div>
-                <Col xs="12" sm="4" md="3" className="options-wrapper ml-auto mt-auto pb-4">
+                <Col
+                  xs="12"
+                  sm="4"
+                  md="3"
+                  className="options-wrapper ml-auto mt-auto pb-4"
+                >
                   <Row className="align-items-center justify-content-end">
                     <Button className="px-2">
                       <FontAwesomeIcon icon={faVideo} />
@@ -56,24 +98,62 @@ const ChatWindow = ({ selectedFriend, handleSubmitChatMessage, messages}) => {
                   </Row>
                 </Col>
               </Row>
-            </Card.Header>
-            <Card.Body className="messages-window">
-              {messages.length > 0 &&
-                messages.map((m, index) => (
-                  <div key={index}>
-                    <MessageCard
-                      text={m.message}
-                      sender={userInfo.auth_id === m.from}
-                      avatar={
-                        userInfo.auth_id === m.from
-                          ? userInfo.avatar
-                          : selectedFriend.friend.avatar
+              {selectedFriend.status === "pending" && (
+                <Toast show={showToast}>
+                  <Toast.Body>
+                    <p>
+                      {selectedFriend.friend.username +
+                        " has sent you a friend request"}
+                    </p>
+                    <Button
+                      size="sm"
+                      onClick={() =>
+                        handleClickAddFriend(
+                          userInfo.auth_id,
+                          selectedFriend._id
+                        )
                       }
-                      messageTime={m.created_at}
-                    />
-                  </div>
-                ))}
-              <div ref={lastMessage} />
+                    >
+                      Accept
+                    </Button>
+                    <Button
+                      size="sm"
+                      className="ml-2"
+                      variant="secondary"
+                      onClick={() =>
+                        handleClickCancelFriend(
+                          userInfo.auth_id,
+                          selectedFriend._id
+                        )
+                      }
+                    >
+                      Cancel
+                    </Button>
+                  </Toast.Body>
+                </Toast>
+              )}
+            </Card.Header>
+
+            <Card.Body className="messages-window">
+              
+              <div className="messages">
+                {messages.length > 0 &&
+                  messages.map((m, index) => (
+                    <div key={index}>
+                      <MessageCard
+                        text={m.message}
+                        sender={userInfo.auth_id === m.from}
+                        avatar={
+                          userInfo.auth_id === m.from
+                            ? userInfo.avatar
+                            : selectedFriend.friend.avatar
+                        }
+                        messageTime={m.created_at}
+                      />
+                    </div>
+                  ))}
+                <div ref={lastMessage} />
+              </div>
             </Card.Body>
             <Card.Footer>
               <Form autoComplete="off" onSubmit={handleSubmitMessage}>
@@ -107,7 +187,6 @@ const ChatWindow = ({ selectedFriend, handleSubmitChatMessage, messages}) => {
             </Card.Footer>
           </Card>
         </Col>
-      )}
     </>
   );
 };
@@ -115,7 +194,9 @@ const ChatWindow = ({ selectedFriend, handleSubmitChatMessage, messages}) => {
 ChatWindow.propTypes = {
   selectedFriend: PropTypes.object,
   handleSubmitChatMessage: PropTypes.func,
-  messages: PropTypes.array
+  messages: PropTypes.array,
+  handleAddFriend: PropTypes.func,
+  handleCancelFriend: PropTypes.func,
 };
 
 export default React.memo(ChatWindow);
