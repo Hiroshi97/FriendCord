f26ee652271c35f8aa2fdbb29762f156f448d181
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-09-18 00:39:05 +1000

    Implement handleCancelFriend and check if the receiveMsg is from a conversation of user and selected friend

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -18,8 +18,10 @@ function Chat() {
     JSON.parse(localStorage.getItem("friends")) || []
   );
   const [messageInput, setMessageInput] = useState("");
-  const [selectedFriend, setSelectedFriend] = useState(friends[0] || "");
-
+  const [selectedFriend, setSelectedFriend] = useState(
+    friends.length > 0 ? friends[0] : null
+  );
+  const messages = useSelector((state) => state.chatsState.messages);
 
   useEffect(() => {
     if (selectedFriend) {
@@ -40,13 +42,36 @@ function Chat() {
           dispatch(getMessages([...response.data.messages]));
         });
 
-      socket.on("result", (data) => {
-        dispatch(postMessage(data));
+      socket.on("receiveMsg", (data) => {
+        if ((selectedFriend._id === data.from && userInfo.auth_id === data.to) || (selectedFriend._id === data.to && userInfo.auth_id === data.from))
+          dispatch(postMessage(data));
       });
-
-      return () => socket.disconnect();
     }
+
+    return () => socket.disconnect();
   }, [messageInput, selectedFriend]);
+
+  useEffect(() => {
+    socket.on("updateFriend", (data) => {
+      axios
+        .post("user/friends", { username: userInfo.username })
+        .then((res) => {
+          localStorage.setItem("friends", JSON.stringify(res.data.friendships));
+          setFriends([...res.data.friendships]);
+        });
+    });
+  });
+
+  const handleAddFriend = useCallback((id1, id2) => {
+    socket.emit("addFriend", { id1, id2 });
+    setMessageInput("");
+  });
+
+  const handleCancelFriend = useCallback((id1, id2) => {
+    socket.emit("cancelFriend", { id1, id2 });
+    setMessageInput("");
+    if (selectedFriend && selectedFriend._id === id2) setSelectedFriend(null);
+  });
 
   const handleSelectFriend = useCallback((friend) => {
     setSelectedFriend(friend);
@@ -54,7 +79,7 @@ function Chat() {
 
   const handleSubmitChatMessage = useCallback((e) => {
     e.preventDefault();
-    socket.emit("message", {
+    socket.emit("sendMsg", {
       from: userInfo.auth_id,
       to: selectedFriend.friend._id,
       message: e.target.messageInput.value,
@@ -70,14 +95,17 @@ function Chat() {
           userInfo={userInfo}
           friends={friends}
           handleSelectFriend={handleSelectFriend}
+          handleAddFriend={handleAddFriend}
+          handleCancelFriend={handleCancelFriend}
         />
         <ChatWindow
           selectedFriend={selectedFriend}
           handleSubmitChatMessage={handleSubmitChatMessage}
+          messages={messages}
         />
       </Row>
     </Container>
   );
 }
 
-export default React.memo(Chat);
+export default Chat;
