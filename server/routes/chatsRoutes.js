5d0d41122287977d1cb4dc6855bfa47f390e5b09
Andy <39031279+Hiroshi97@users.noreply.github.com>
2020-09-19 12:26:30 +1000

    Implement route to get last messages

--- server/routes/chatsRoutes.js
+++ server/routes/chatsRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/userModel");
 const Message = require("../models/messageModel");
+const { createUniqueID } = require("../utils/create-unique-id");
 
 //ROUTES
 
@@ -12,6 +13,7 @@ router.post("/getMessages", async (req, res) => {
       req.headers.authorization === req.body.id2 + req.body.id2.slice(-3)
     ) {
       const { id1, id2 } = req.body;
+      //Get all messages of friends
       const messages = await Message.find({
         $or: [
           { from: id1, to: id2 },
@@ -24,5 +26,38 @@ router.post("/getMessages", async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 });
+
+router.post("/getLastMessages", async (req, res) => {
+  try {
+    if (req.headers.authorization === req.body.id + req.body.id.slice(-3)) {
+      const { id } = req.body;
+      const user = await User.findOne(
+        { _id: id },
+        { name: 1, username: 1, avatar: 1, email: 1 }
+      );
+
+      const lastMessages = await Message.aggregate([
+        { $match: {$or: [{from: id}, {to: id}]}},
+        { $sort: { created_at: -1 } },
+        {
+          $group: {
+            _id: "$uid",
+            message: { $first: "$message" },
+            from: { $first: "$from" },
+            to: { $first: "$to" },
+            time: { $first: "$created_at"}
+          },
+        },
+      ])
+
+      return res.status(200).json({ lastMessages });
+
+    } else return res.status(500).json("Unauthorized");
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+//Get last messages
 
 module.exports = router;
